0cb333653f2f881febb888e25ea824dadea3229b
Johnny <johnnyy.yu@gmail.com>
2021-12-02 16:42:19 -0800

    Added UI

--- script.js
+++ script.js
@@ -13,9 +13,7 @@ function computerPlay() {
     return cpuPlay;
 }
 
-function playRound(playerSelection = window.prompt("Choose Rock, Paper, or Scissors"), computerSelection = computerPlay()) {
-    playerSelection = playerSelection.toLowerCase();
-
+function playRound(playerSelection, computerSelection = computerPlay()) {
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             return "You Lose! Paper beats Rock";
@@ -39,28 +37,58 @@ function playRound(playerSelection = window.prompt("Choose Rock, Paper, or Sciss
     return "You Tie! Go again!"
 } 
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
+function game(playerSelection) {
+    let results = playRound(playerSelection);
+        div.textContent = results;
+        container.appendChild(div);
 
-    for (let i = 0; i < 5; i++) {
-        let results = playRound();
-        console.log(results);
+    if (results.includes("Win!")) {
+        playerWins += 1;
+    } else if (results.includes("Lose!")) {
+        computerWins += 1;
+    }
 
-        if (results.includes("Win!")) {
-            playerWins += 1;
-        } else if (results.includes("Lose!")) {
-            computerWins += 1;
+    score = "Score is " + playerWins + ":" + computerWins;
+    div2.textContent = score;
+    container.appendChild(div2);
+
+    if (playerWins == 5 || computerWins == 5) {
+
+        if (playerWins > computerWins) {
+            div.textContent = "You are the Winner!";
+        } else {
+            div.textContent = "The Computer is the Winner!";
         }
 
-        console.log("Score is " + playerWins + ":" + computerWins);
-    }
-
-    if (playerWins > computerWins) {
-        console.log("You are the Winner!");
-    } else if (computerWins > playerWins) {
-        console.log("The Computer is the Winner!");
-    } else {
-        console.log("Tie Game! No one is the Winner!")
+        container.appendChild(div);
+        container.appendChild(div2);
+        playerWins = 0;
+        computerWins = 0;
     }
 }
+
+let playerWins = 0;
+let computerWins = 0;
+
+const container = document.querySelector("#results");
+const div = document.createElement("div");
+const div2 = document.createElement("div");
+
+const rockButton = document.querySelector('.rock');
+
+rockButton.addEventListener("click", () => {
+    game("rock");
+})
+
+const paperButton = document.querySelector(".paper");
+
+paperButton.addEventListener("click", () => {
+    game("paper")
+})
+
+const scissorsButton = document.querySelector(".scissors");
+
+scissorsButton.addEventListener("click", () => {
+    game("scissors")
+})
+
